654885424bad8e8e1910d6d5ba5bdbdeb33ebedc
Pulzze92 <76836870+Pulzze92@users.noreply.github.com>
2024-11-17 23:17:05 +0100

    fix: some fixes

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import {Button, Card, Form, Input, message} from 'antd';
+import {Button, Card, Checkbox, Form, Input, message} from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { login } from "../../store/slices/authSlice";
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from "../../store";
 import {AuthResponse} from "../../types/auth";
+
+type FieldType = {
+    Username?: string;
+    Password?: string;
+    Remember?: string;
+};
 
 export const Login: React.FC = () => {
     const [error, setError] = useState<string | null>(null);
@@ -30,15 +36,21 @@ export const Login: React.FC = () => {
             margin: '100px auto',
             padding: '0 16px'}}>
             <Form onFinish={handleLogin}>
-                <Form.Item name="username" rules={[{required: true, message: 'Enter your username'}]}>
+                <Form.Item<FieldType> name="Username" rules={[{required: true, message: 'Enter your username'}]}>
                     <Input placeholder="Username" />
                 </Form.Item>
-                <Form.Item name="Password" rules={[{required: true, message: 'Enter your password'}]}>
+                <Form.Item<FieldType> name="Password" rules={[{required: true, message: 'Enter your password'}]}>
                     <Input.Password placeholder="Password" />
                 </Form.Item>
+                <Form.Item<FieldType> name="Remember" valuePropName="checked" label={null}>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
                 {error && <p style={{color: 'red'}}>{error}</p>}
-                <Button type="primary" htmlType="submit">Login</Button>
-                <Button type="link" onClick={() => navigate('/register')}>Register</Button>
+                <div style={{ textAlign: 'center' }}>
+                    <Button type="primary" htmlType="submit">Login</Button>
+                    <Button type="link" onClick={() => navigate('/register')}>Register</Button>
+                </div>
+
             </Form>
         </Card>
     )
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import {Button, Card, Form, Input, message} from "antd";
+import { Button, Card, Form, Input, message, Checkbox } from "antd";
 import {useNavigate} from "react-router-dom";
 import {RegisterRequest} from "../../types/auth";
 import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch } from "../../store";
+import {AppDispatch, RootState} from "../../store";
 import { register } from "../../store/slices/authSlice";
+import { LockOutlined } from '@ant-design/icons';
+
+type FieldType = {
+    Username?: string;
+    Password?: string;
+    ConfirmPassword?: string;
+    Remember?: string;
+};
 
 export const Register: React.FC = () => {
     const [error, setError] = React.useState<string | null>(null);
     const navigate = useNavigate();
     const dispatch = useDispatch<AppDispatch>();
+    const { isLoading } = useSelector((state: RootState) => state.auth);
 
     const handleSignUp = async (values: RegisterRequest) => {
         try {
@@ -29,16 +38,46 @@ export const Register: React.FC = () => {
             maxWidth: '400px',
             margin: '100px auto',
             padding: '0 16px'}}>
-            <Form onFinish={handleSignUp}>
-                <Form.Item name="username" rules={[{required: true, message: 'Enter your username'}]}>
+            <Form
+                onFinish={handleSignUp}
+                autoComplete="off"
+                layout="vertical"
+            >
+                <Form.Item<FieldType> name="Username" rules={[{required: true, message: 'Enter your username'}]}>
                     <Input placeholder="Username" />
                 </Form.Item>
-                <Form.Item name="Password" rules={[{required: true, message: 'Enter your password'}]}>
+                <Form.Item<FieldType> name="Password" rules={[{required: true, message: 'Enter your password'}]}>
                     <Input.Password placeholder="Password" />
                 </Form.Item>
+                <Form.Item<FieldType> name="ConfirmPassword"
+                                      dependencies={['password']}
+                                      rules={[
+                    { required: true, message: 'Confirm your password' },
+                    ({ getFieldValue }) => ({
+                        validator(_, value) {
+                            if (!value || getFieldValue('password') === value) {
+                                return Promise.resolve();
+                            }
+                            return Promise.reject(new Error('Passwords do not match'));
+                        },
+                    }),
+                ]}
+                >
+                    <Input.Password
+                        prefix={<LockOutlined />}
+                        placeholder="Confirm Password"
+                    />
+                </Form.Item>
+                <Form.Item<FieldType> name="Remember" valuePropName="checked" label={null}>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
                 {error && <p style={{color: 'red'}}>{error}</p>}
-                <Button type="primary" htmlType="submit">Sign Up</Button>
-                <Button type="link" onClick={() => navigate('/login')}>Login</Button>
+                <Form.Item style={{ textAlign: 'center' }}>
+                    <Button type="primary" htmlType="submit" loading={isLoading}>Sign Up</Button>
+                </Form.Item>
+                <Form.Item style={{ textAlign: 'center' }}>
+                    Already have account? <Button type="link" onClick={() => navigate('/login')}>Login</Button>
+                </Form.Item>
             </Form>
         </Card>
     )
